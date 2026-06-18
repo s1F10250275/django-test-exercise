05cf79d4e80ca1569a6ff625b9ac2db82cce52a4
1F10250275 <s1F102502755@iniad.org>
2026-06-18 14:34:56 +0900

    ソート機能を追加

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,25 @@
-from django. shortcuts import render
-from django. utils. timezone import make_aware
-from django. utils.dateparse import parse_datetime 
-from todo. models import Task
+from django.shortcuts import render
+from django.utils.timezone import make_aware
+from django.utils.dateparse import parse_datetime
+from todo.models import Task
+
 
 # Create your views here.
-def index(request) :
-    if request.method == 'POST':
-        task = Task(title=request. POST[ ' title' ],
-            due_at=make_aware(parse_datetime(request. POST[ 'due_at' ])))
-        task. save()
+def index(request):
+    if request.method == "POST":
+        task = Task(
+            title=request.POST["title"],
+            due_at=make_aware(parse_datetime(request.POST["due_at"])),
+        )
+        task.save()
 
-    tasks = Task. objects. all()
+    # ここで条件に応じた並び替えを行う
+    if request.GET.get("order") == "due":
+        tasks = Task.objects.order_by("due_at")
+    else:
+        tasks = Task.objects.order_by("-posted_at")
 
-    context = {
-        'tasks': tasks
-    }
-    return render(request, 'todo/index.html', context)
+    # 【修正箇所】ここに写っていた「tasks = Task.objects.all()」を削除しました！
+
+    context = {"tasks": tasks}
+    return render(request, "todo/index.html", context)
